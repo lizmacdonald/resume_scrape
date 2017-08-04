efc84482e6bb6490c4c2a65354cbeeef85676297
Liz M <lizmacdonald45@gmail.com>
2017-08-04 16:53:08 -0600

    adding in identification of ddegree and university where degree was obtained

--- pdf_parsing_code.py
+++ pdf_parsing_code.py
@@ -3,6 +3,7 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from cStringIO import StringIO
+import re
 
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager() 
@@ -20,3 +21,43 @@ def convert_pdf_to_txt(path):
 
 
 plain_text = convert_pdf_to_txt("MacDonald_CV.pdf")
+
+### set up list to capture lines in pdf
+lines = []
+clean = []
+
+### select pattern you want to search for
+pattern = 'Ph.D.'  ##set this upto be the list of degrees
+spattern = 'Johns Hopkins'
+
+
+### separate each item in the list by a linebreak
+for text in plain_text.split("\n"):
+    lines.append(text)
+
+## remove any items in the list that are merely spaces or commas
+for line in lines:
+    if line != '' and line != ' ':
+        clean.append(line)
+
+
+## find the indicies in clean that contain info found in pattern
+indices = [i for i, s in enumerate(clean) if pattern in s]
+sindices = [i for i, s in enumerate(clean) if spattern in s]
+
+### find the line previous and the line after the idicies that contain the info i want
+## find the items in clean with the indicies at those three spots
+ed_list = []
+'''for i in indices:
+    if i > 0:
+        ed_list.append(clean[i])
+        ed_list.append(clean[i+1])
+        ed_list.append(clean[i-1])
+'''
+
+
+
+
+
+
+
